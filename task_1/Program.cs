684f016f764e3d84d27df6c983231592730f4c57
Taras <taras.peschenko.90@mail.ru>
2022-12-13 20:24:36 +0300

    заполнить двумерный массив веществен числами округ до одного знака

---
+++ task_1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами,
+// округлёнными до одного знака.
+
+void Task_1()
+{
+    int rows = 3;
+    int culumns = 4;
+    double[,] numbers = new double[rows, culumns];
+
+    FillArray(numbers);
+    PrintArray(numbers);
+}
+
+void FillArray(double[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int culumns = numbers.GetLength(1);
+    Random rand = new Random();
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < culumns; j++)
+        {
+            numbers[i, j] = Math.Round(rand.NextDouble()*20 - 10, 1);
+            
+        } 
+    }
+}
+void PrintArray(double[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int culumns = numbers.GetLength(1);
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < culumns; j++)
+        {
+            Console.Write(numbers[i, j] + " ");
+        }
+        Console.WriteLine(""); 
+    }
+}
+Task_1();
